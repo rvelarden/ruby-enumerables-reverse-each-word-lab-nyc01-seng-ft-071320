d645d19c7aac6250eaad6c79135ee4490dba3f5e
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-20 00:34:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 requiering 'pry'
 def reverse_each_word (sentence)
   binding.pry 
+  sentence_array= sentence.split(" ")
+  new_array=[]
   
-  
-  
+  new_array<< sentence_array.each do {|element| element.reverse!}.join(" ")
 end   
+end
