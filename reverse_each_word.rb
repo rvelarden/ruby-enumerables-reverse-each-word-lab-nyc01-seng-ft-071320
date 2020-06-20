11204e2683f25afc4ca974216ecd287cbeb2a16f
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-20 00:43:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 sentence = "Hello there, my friend."
 
 def reverse_each_word(sentence)
-  word_array = sentence.split
+  word_array = sentence.collect
   new_sentence = []
   word_array.each do |word|
     rev_word = word.reverse
