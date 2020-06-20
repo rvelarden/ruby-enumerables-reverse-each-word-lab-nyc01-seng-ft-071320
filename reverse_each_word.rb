f4b807f41ba902357a3d41aad3ed7d366998c071
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-20 00:50:41 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+require 'pry'
+sentence = "Hello there, my friend."
+
+def reverse_each_word(sentence)
+  word_array = sentence.split
+  new_sentence = []
+  word_array.collect do |word|
+    rev_word = word.reverse
+    new_sentence << rev_word
+    
+  end
+result = new_sentence.join(" ")
+result
+end
+
+
+reverse_each_word(sentence)
