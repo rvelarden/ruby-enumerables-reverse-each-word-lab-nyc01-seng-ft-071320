56b5b7d2955d7d16b4429e14a2a47ff18b8ab1fb
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-20 00:50:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,10 +4,10 @@ sentence = "Hello there, my friend."
 def reverse_each_word(sentence)
   word_array = sentence.split
   new_sentence = []
-  word_array.each do |word|
+  word_array.collect do |word|
     rev_word = word.reverse
     new_sentence << rev_word
-    word_array.collect 
+    
   end
 result = new_sentence.join(" ")
 result
