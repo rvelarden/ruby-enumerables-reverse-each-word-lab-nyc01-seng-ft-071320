03abb02a1ce957b0074b27ac17e2217cfd6e153c
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-20 00:23:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,7 @@
 def  reverse_each_word(string)
-  
+sentence_array= sentence.split(" ")   
 string.each do |string|
   if string.collect 
-    
-    def key_for_min_value(hash)
-  lowest_key = nil
-  lowest_value = nil
-  hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v
-      lowest_key = k
-    end
-  end
-  lowest_key
+   
+   
 end
