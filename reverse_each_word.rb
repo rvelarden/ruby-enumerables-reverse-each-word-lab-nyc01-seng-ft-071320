9dd9e1488aa43486aab4bc07038f94bf86d35654
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-20 00:30:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (sentence)
   sentence_array= sentence.split(" ")
-  
+  new_array=[ ]
   
   new_array<< sentence_array.each {|element| element.reverse!}.join(" ")
-  new_array=[ ]
+  
 end
